79aee150da85750a5c9e9f135bce660484b3e6a9
springlordpants <springlordpants@gmail.com>
2020-11-17 15:30:00 -0800

    Added several functions

--- src/App.js
+++ src/App.js
@@ -22,11 +22,51 @@ const auth = firebase.auth()
 const firestore = firebase.firestore()
 
 function App() {
+  const [user] = useAuthState(auth)
+
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header></header>
+      <section>{user ? <ChatRoom /> : <SignIn />}</section>
     </div>
   )
+}
+
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider()
+    auth.signInWithPopup(provider)
+  }
+
+  return <button onClick={signInWithGoogle}>Sign in with Google</button>
+}
+
+function SignOut() {
+  return (
+    auth.currentUser && <button onClick={() => auth.signOut()}>Sign Out</button>
+  )
+}
+
+function ChatRoom() {
+  const messagesRef = firestore.collection("messages")
+  const query = messagesRef.orderBy("createdAt").limit(25)
+
+  const [messages] = useCollectionData(query, { idField: "id" })
+
+  return (
+    <>
+      <div>
+        {messages &&
+          messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
+      </div>
+      <div></div>
+    </>
+  )
+}
+
+function ChatMessage(props) {
+  const { text, uid } = props.message
+  return <p>{text}</p>
 }
 
 export default App
