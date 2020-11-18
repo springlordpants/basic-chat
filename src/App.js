8922b9977242b4eec9efbfcff0abca9787872975
springlordpants <springlordpants@gmail.com>
2020-11-17 16:25:49 -0800

    added useState

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import "./App.css"
 
 import firebase from "firebase/app"
@@ -53,21 +53,21 @@ function ChatRoom() {
 
   const [messages] = useCollectionData(query, { idField: "id" })
 
-  const [formValue, setFormValue] = useState('')
+  const [formValue, setFormValue] = useState("")
 
-  const sendMessage = async(e) => {
-    e.preventDefault();
+  const sendMessage = async (e) => {
+    e.preventDefault()
 
-    const {uid, photoURL} = auth.currentUser
-    
+    const { uid, photoURL } = auth.currentUser
+
     await messagesRef.add({
-      text:formValue,
+      text: formValue,
       createdAt: firebase.firestore.FieldValue.serverTimestamp(),
       uid,
-      photoURL
+      photoURL,
     })
-    
-    setFormValue('')
+
+    setFormValue("")
   }
 
   return (
@@ -77,7 +77,10 @@ function ChatRoom() {
           messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
       </div>
       <form onSubmit={sendMessage}>
-        <input value={formValue} onChange={(e) => setFormValue(e.target.value)} />
+        <input
+          value={formValue}
+          onChange={(e) => setFormValue(e.target.value)}
+        />
         <button type="submit">Submit</button>
       </form>
     </>
@@ -85,15 +88,15 @@ function ChatRoom() {
 }
 
 function ChatMessage(props) {
-  const { text, uid } = props.message
+  const { text, uid, photoURL } = props.message
 
   const messageClass = uid === auth.currentUser.uid ? "sent" : "received"
 
   return (
-    <div className = {`message ${messageClass}`}>
+    <div className={`message ${messageClass}`}>
       <img src={photoURL} />
       <p>{text}</p>
     </div>
   )
-
+}
 export default App
